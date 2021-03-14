1529e83b6bca39949b40fb1764d3044d8719b6ae
aguscuk <aguscuk@gmail.com>
2021-03-14 14:54:07 +0700

    Update nfs-mount.sh

--- nfs-mount.sh
+++ nfs-mount.sh
@@ -10,7 +10,7 @@ ISMOUNTED=`grep "$dest" /proc/mounts`
 
 if [ "$ISMOUNTED" = "" ]
 then
-  sudo /bin/mount -t nfs $source $dest
+  sudo /bin/mount -t nfs -o rw,sync,vers=4,timeo=60,_netdev $source $dest
   echo "Berhasil di-mounting  : `grep $dest /proc/mounts`"
 else
   echo "Sudah di-mounting  : $ISMOUNTED"
